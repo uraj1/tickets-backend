98d3f73d3c9369656edb213bd0f393698defffae
Sanniv2002 <sanniv.1020@gmail.com>
2025-02-02 16:21:45 +0530

    fix cookie

--- src/middleware/session.ts
+++ src/middleware/session.ts
@@ -6,7 +6,7 @@ export const sessionMiddleware = session({
   saveUninitialized: false, 
   cookie: {
     maxAge: 1000 * 60 * 60 * 24, 
-    httpOnly: true,
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     secure: false, 
   }
 })

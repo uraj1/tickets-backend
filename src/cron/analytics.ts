e32f8875332205895a39c57f1594c5d7c7819ece
Sanniv2002 <sanniv.1020@gmail.com>
2025-03-15 18:41:12 +0530

    removed success log from analytics

--- src/cron/analytics.ts
+++ src/cron/analytics.ts
@@ -4,7 +4,6 @@ import { logger } from "../services/logger.service";
 export const runAnalyticsScheduler = async () => {
   try {
     await saveAnalytics();
-    logger.info(`Succesfully saved analytics`);
   } catch (error) {
     logger.info(`Error in saving analytics`, error);
     console.error("Error running analytics scheduler:", error);
